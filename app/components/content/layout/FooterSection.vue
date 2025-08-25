97f658944e16fb4a2b90e10ed800067a494bf4ba
banedPowell <68949513+banedPowell@users.noreply.github.com>
2025-08-25 19:06:40 +0200

    fix: suppression de la classe de debug

--- app/components/content/layout/FooterSection.vue
+++ app/components/content/layout/FooterSection.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-	<ul class="FOOTERSECTION flex flex-col items-start justify-start gap-1">
+	<ul class="flex flex-col items-start justify-start gap-1">
 		<li>
 			<h4 class="mb-1.5 font-black">
 				<slot name="title" />
