30b1ec4b7fed7b2c83e54fc7db0b3777a24d3451
banedPowell <68949513+banedPowell@users.noreply.github.com>
2025-09-25 01:42:27 +0200

    refactor: simplification des props dans ProjectCard.vue en remplaçant l'objet project par des props individuelles

--- app/components/content/ProjectCard.vue
+++ app/components/content/ProjectCard.vue
@@ -1,13 +1,9 @@
 <script lang="ts" setup>
 	const props = defineProps<{
-		project: {
-			title: string;
-			description: string;
-			projectIcon: string;
-			iconAlt: string;
-			slug: string;
-			repoUrl: string;
-		};
+		projectIcon: string;
+		iconAlt: string;
+		slug: string;
+		repoUrl: string;
 	}>();
 
 	import { useWindowSize } from '@vueuse/core';
@@ -18,13 +14,13 @@
 
 <template>
 	<li>
-		<NuxtLink :to="`/projects/${project.slug}`" v-if="!mdScreenSize">
+		<NuxtLink :to="`/projects/${slug}`" v-if="!mdScreenSize">
 			<div
 				class="border-dark-800 hover:bg-dark-800 flex w-full flex-col gap-3 rounded-xl border-2 p-5"
 			>
 				<div class="flex w-full flex-row items-center justify-between">
 					<NuxtImg
-						:src="project.projectIcon"
+						:src="projectIcon"
 						alt="Logo de Deezerstats"
 						class="aspect-square size-8"
 					/>
@@ -47,16 +43,16 @@
 			class="border-dark-800 flex w-full flex-col gap-3 rounded-xl border-1 p-5"
 		>
 			<div class="flex w-full flex-row items-center justify-between">
-				<NuxtLink :to="`/projects/${project.slug}`" class="w-full">
+				<NuxtLink :to="`/projects/${slug}`" class="w-full">
 					<NuxtImg
-						:src="project.projectIcon"
-						:alt="project.iconAlt"
+						:src="projectIcon"
+						:alt="iconAlt"
 						class="aspect-square size-8"
 					/>
 				</NuxtLink>
 
 				<NuxtLink
-					:to="project.repoUrl"
+					:to="repoUrl"
 					target="_blank"
 					class="flex flex-row items-center gap-2 rounded-md p-3 pt-1 pb-1 text-gray-400 transition-colors hover:bg-gray-400/10"
 				>
@@ -66,14 +62,13 @@
 				</NuxtLink>
 			</div>
 
-			<NuxtLink
-				:to="`/projects/${project.slug}`"
-				class="flex flex-col gap-1.5"
-			>
-				<h3 class="text-lg text-gray-300">{{ project.title }}</h3>
+			<NuxtLink :to="`/projects/${slug}`" class="flex flex-col gap-1.5">
+				<h3 class="text-lg text-gray-300">
+					<slot name="title" />
+				</h3>
 
 				<p class="text-gray-500">
-					{{ project.description }}
+					<slot name="description" />
 				</p>
 			</NuxtLink>
 		</div>
