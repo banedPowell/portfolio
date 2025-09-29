c8e237aee0b515bdaf563b46bc929043a916d696
banedPowell <68949513+banedPowell@users.noreply.github.com>
2025-09-29 18:38:56 +0200

    refactor: suppression des propriétés 'externalLink' et 'target' dans LayoutItem.vue pour utiliser les prose components

--- app/components/content/layout/LayoutItem.vue
+++ app/components/content/layout/LayoutItem.vue
@@ -1,26 +1,13 @@
 <script lang="ts" setup>
 	export interface FooterItem {
 		icon?: string;
-		externalLink?: string;
-		target?: '_self' | '_blank' | '_parent' | '_top';
 	}
 
 	const props = defineProps<FooterItem>();
 </script>
 
 <template>
-	<li v-if="externalLink" class="list-none">
-		<NuxtLink
-			:to="externalLink"
-			:target="target"
-			class="group/islink flex flex-nowrap items-center gap-2 transition-colors hover:text-gray-300"
-		>
-			<Icon v-if="icon" :name="icon" />
-			<slot />
-		</NuxtLink>
-	</li>
-
-	<li v-else class="flex list-none flex-nowrap items-center gap-2">
+	<li class="flex list-none flex-nowrap items-center gap-2">
 		<Icon v-if="icon" :name="icon" />
 		<slot />
 	</li>
