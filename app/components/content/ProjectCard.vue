14a0748e5ef858723a3d85a71337b7e8eceefb26
banedPowell <68949513+banedPowell@users.noreply.github.com>
2025-09-30 00:38:37 +0200

    refactor: simplification et amélioration du composant ProjectCard.vue avec des ajustements de style et de structure pour une meilleure expérience utilisateur

--- app/components/content/ProjectCard.vue
+++ app/components/content/ProjectCard.vue
@@ -5,60 +5,30 @@
 		slug: string;
 		repoUrl: string;
 	}>();
-
-	import { useWindowSize } from '@vueuse/core';
-
-	const { width } = useWindowSize();
-	const mdScreenSize = computed(() => width.value >= 640);
 </script>
 
 <template>
-	<li>
-		<NuxtLink :to="`/projects/${slug}`" v-if="!mdScreenSize">
-			<div
-				class="border-dark-800 hover:bg-dark-800 flex w-full flex-col gap-3 rounded-xl border-2 p-5"
-			>
-				<div class="flex w-full flex-row items-center justify-between">
-					<NuxtImg
-						:src="projectIcon"
-						alt="Logo de Deezerstats"
-						class="aspect-square size-8"
-					/>
-				</div>
+	<li class="group relative">
+		<Icon
+			name="lucide:arrow-up-right"
+			class="absolute top-5 right-5 transition-transform group-hover:translate-x-1 group-hover:-translate-y-1"
+		/>
 
-				<div class="flex flex-col">
-					<slot />
-				</div>
-			</div>
-		</NuxtLink>
-
-		<div
-			v-else
-			class="border-dark-800 flex w-full flex-col gap-3 rounded-xl border-1 p-5"
+		<NuxtLink
+			:to="`/projects/${slug}`"
+			class="border-dark-800 hover:border-dark-600 flex w-full flex-col gap-3 rounded-xl border-2 p-5 transition-colors"
 		>
 			<div class="flex w-full flex-row items-center justify-between">
-				<NuxtLink :to="`/projects/${slug}`" class="w-full">
-					<NuxtImg
-						:src="projectIcon"
-						:alt="iconAlt"
-						class="aspect-square size-8"
-					/>
-				</NuxtLink>
-
-				<NuxtLink
-					:to="repoUrl"
-					target="_blank"
-					class="flex flex-row items-center gap-2 rounded-md p-3 py-1 text-gray-400 transition-colors hover:bg-gray-400/10"
-				>
-					<Icon name="ion:logo-github" class="text-gray-400" />
-
-					Github
-				</NuxtLink>
+				<NuxtImg
+					:src="projectIcon"
+					alt="Logo de Deezerstats"
+					class="aspect-square size-8"
+				/>
 			</div>
 
-			<NuxtLink :to="`/projects/${slug}`" class="flex flex-col gap-1.5">
+			<div class="flex flex-col">
 				<slot />
-			</NuxtLink>
-		</div>
+			</div>
+		</NuxtLink>
 	</li>
 </template>
