c7c02378148929672ee653b6cdcba66f4258cd4b
banedPowell <68949513+banedPowell@users.noreply.github.com>
2025-09-30 00:40:19 +0200

    feat: ajout du composant ProjectsIllustrations.vue pour afficher les illustrations des projets

---
+++ app/components/content/ProjectsIllustrations.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+	const props = defineProps<{
+		illustrations: [string, string, string];
+		alt: [string, string, string];
+	}>();
+</script>
+
+<template>
+	<div class="flex flex-row gap-4">
+		<NuxtImg
+			v-for="(illustration, index) in props.illustrations"
+			:src="illustration"
+			:alt="props.alt[index]"
+			class="aspect-square size-45 rounded-lg"
+		/>
+	</div>
+</template>
