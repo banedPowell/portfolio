dd8b9f3660d625de2cdda91105d9c01a2ea8bdfc
banedPowell <68949513+banedPowell@users.noreply.github.com>
2025-09-30 23:14:38 +0200

    feat: ajout du composant ContentToc et utilisation des composants NuxtUI UPage, UPageBody et UPageAside

--- app/pages/projects/[slug].vue
+++ app/pages/projects/[slug].vue
@@ -7,28 +7,43 @@
 </script>
 
 <template>
-	<div class="flex w-full flex-col gap-20">
-		<ULink
-			to="/"
-			class="bg-dark-800 hover:bg-dark-700 flex w-fit flex-row items-center gap-2 rounded-md px-5 py-2 text-gray-500 hover:text-gray-300"
-		>
-			<Icon name="lucide:arrow-left" /> Retour
+	<UPage class="w-full">
+		<ULink to="/" class="w-fit">
+			<UButton
+				variant="ghost"
+				class="w-fit cursor-pointer hover:text-gray-300"
+			>
+				<Icon name="lucide:chevron-left" /> Retour
+			</UButton>
 		</ULink>
 
-		<template v-if="project">
-			<ContentRenderer
-				class="flex w-full flex-col gap-20"
-				:value="project"
-			/>
-		</template>
+		<UPageBody>
+			<template v-if="project">
+				<ContentRenderer
+					class="flex w-full flex-col gap-20"
+					:value="project"
+				/>
+			</template>
 
-		<template v-else>
-			<div class="empty-page">
-				<h1 class="text-2xl font-bold">Page Not Found</h1>
-				<p class="text-gray-500">
-					Oops! The content you're looking for doesn't exist.
-				</p>
-			</div>
+			<template v-else>
+				<div class="empty-page w-full">
+					<h1 class="text-2xl text-gray-300">Projet introuvable</h1>
+					<p class="text-gray-500">
+						Le projet que vous cherchez n'existe pas.
+					</p>
+				</div>
+			</template>
+		</UPageBody>
+
+		<template #right>
+			<UPageAside>
+				<UContentToc
+					:links="project?.body?.toc?.links"
+					title="Sur cette page"
+					:ui="{ title: 'text-gray-400' }"
+					class="w-fit"
+				/>
+			</UPageAside>
 		</template>
-	</div>
+	</UPage>
 </template>
