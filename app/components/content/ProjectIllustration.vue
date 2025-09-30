c0d7c5c1e6990602c1e59dfe6123dfadd33c3119
banedPowell <68949513+banedPowell@users.noreply.github.com>
2025-09-30 15:23:44 +0200

    refactor: Renommage de ProjectIllustrationsList.vue et réorganisation de l'utilisation des illustrations avec ProjectIllustration.vue

---
+++ app/components/content/ProjectIllustration.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+	const props = defineProps<{
+		illustrationSrc: string;
+		illustrationTitle: string;
+		illustrationAlt: string;
+	}>();
+</script>
+
+<template>
+	<li
+		class="border-dark-600 flex aspect-square w-full cursor-zoom-in items-center justify-center overflow-hidden rounded-lg border-0 transition-all odd:rotate-5 even:-rotate-5 hover:scale-105 hover:rotate-0 hover:border-4 sm:w-1/3"
+	>
+		<UModal :title="illustrationTitle" :description="illustrationAlt">
+			<NuxtImg
+				:src="illustrationSrc"
+				:alt="illustrationAlt"
+				class="object-no-repeat h-full w-full object-cover object-center"
+			/>
+
+			<template #body>
+				<NuxtImg
+					:src="illustrationSrc"
+					:alt="illustrationAlt"
+					class="rounded-sm object-contain"
+				/>
+			</template>
+		</UModal>
+	</li>
+</template>
---
+++ app/components/content/ProjectIllustrationsList.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+	const props = defineProps<{
+		firstIllustration: string;
+		firstIllustrationTitle: string;
+		firstIllustrationAlt: string;
+		secondIllustration: string;
+		secondIllustrationTitle: string;
+		secondIllustrationAlt: string;
+		thirdIllustration: string;
+		thirdIllustrationTitle: string;
+		thirdIllustrationAlt: string;
+	}>();
+</script>
+
+<template>
+	<ul
+		class="flex w-full flex-col justify-center gap-5 px-5 sm:flex-row sm:px-0"
+	>
+		<ProjectIllustration
+			:illustrationSrc="firstIllustration"
+			:illustrationTitle="firstIllustrationTitle"
+			:illustrationAlt="firstIllustrationAlt"
+		/>
+
+		<ProjectIllustration
+			:illustrationSrc="secondIllustration"
+			:illustrationTitle="secondIllustrationTitle"
+			:illustrationAlt="secondIllustrationAlt"
+		/>
+
+		<ProjectIllustration
+			:illustrationSrc="thirdIllustration"
+			:illustrationTitle="thirdIllustrationTitle"
+			:illustrationAlt="thirdIllustrationAlt"
+		/>
+	</ul>
+</template>
